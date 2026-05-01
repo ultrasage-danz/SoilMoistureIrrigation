bcd821ff1972db642aac53bd84e0a0a256781182
ultrasage-danz <82732173+ultrasage-danz@users.noreply.github.com>
2026-05-01 23:57:53 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -16,7 +16,7 @@ async def test_project(dut):
     # ── Reset ──────────────────────────────────────────────────────────
     dut._log.info("Reset")
     dut.ena.value    = 1
-    dut.ui_in.value  = 0
+    dut.ui_in.value  = 0b01
     dut.uio_in.value = 0
     dut.rst_n.value  = 0
     await ClockCycles(dut.clk, 10)
